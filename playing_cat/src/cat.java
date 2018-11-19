1d0af90334acb303a31acd8daee0524aeff3ad4d
Kucman Mateusz <mateusz.kucman@igt.com>
2018-11-19 17:28:53 +0100

    Changed cat.

--- playing_cat/src/cat.java
+++ playing_cat/src/cat.java
@@ -1,14 +1,16 @@
 public class cat {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(isCatPlaying(true, 10));
         System.out.println(isCatPlaying(false, 36));
         System.out.println(isCatPlaying(false, 35));
     }
-    private static boolean isCatPlaying(boolean summer, int temperature){
-        if (summer && temperature < 46 && temperature >= 25){
+
+    private static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer && temperature < 46 && temperature >= 25) {
             return true;
-        }else if (!summer && temperature < 36 && temperature >= 25){
+        } else if (!summer && temperature < 36 && temperature >= 25) {
             return true;
-        }return false;
+        }
+        return false;
     }
 }
